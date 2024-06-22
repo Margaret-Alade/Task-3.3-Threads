28333737fa4e357479996b815c2341e57748089d
Margaret Alade <alade.margaret@yandex.ru>
2024-06-22 15:17:33 +0300

    Correct homework 3

--- Task-3.3-Thread/main.cpp
+++ Task-3.3-Thread/main.cpp
@@ -29,21 +29,51 @@ public:
     
 };
 
-
+//при помощи lock
 void swap(Data& data1, Data& data2) {
-    std::unique_lock lk1(data1.m, std::defer_lock);
-    std::unique_lock lk2(data2.m, std::defer_lock);
-    std::lock(lk1, lk2);
+    std::lock(data1.m, data2.m);
+    std::lock_guard lk(data1.m, std::adopt_lock);
+    std::lock_guard lk2(data2.m, std::adopt_lock);
     int tmp1 = data1.num1_;
     data1.num1_= data2.num1_;
     data2.num1_ = tmp1;
     int tmp2 = data1.num2_;
     data1.num2_= data2.num2_;
     data2.num2_ = tmp2;
-    
     data1.print_num();
     data2.print_num();
 }
+
+
+//через std::scoped_lock
+//void swap(Data& data1, Data& data2) {
+//    std::scoped_lock(data1.m, data2.m);
+//    int tmp1 = data1.num1_;
+//    data1.num1_= data2.num1_;
+//    data2.num1_ = tmp1;
+//    int tmp2 = data1.num2_;
+//    data1.num2_= data2.num2_;
+//    data2.num2_ = tmp2;
+//    
+//    data1.print_num();
+//    data2.print_num();
+//}
+
+//через unique_lock
+//void swap(Data& data1, Data& data2) {
+//    std::unique_lock lk1(data1.m, std::defer_lock);
+//    std::unique_lock lk2(data2.m, std::defer_lock);
+//    std::lock(lk1, lk2);
+//    int tmp1 = data1.num1_;
+//    data1.num1_= data2.num1_;
+//    data2.num1_ = tmp1;
+//    int tmp2 = data1.num2_;
+//    data1.num2_= data2.num2_;
+//    data2.num2_ = tmp2;
+//    
+//    data1.print_num();
+//    data2.print_num();
+//}
 
 
 int main(int argc, const char * argv[]) {
